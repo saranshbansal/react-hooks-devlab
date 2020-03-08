47745e4e8465e2de0aeac22c4e0453aa0b2ff70c
Saransh Bansal <saran3h.bansal@gmail.com>
2020-03-08 20:47:51 +0530

    improve login component.

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useFormState } from "../useFormState";
 
 export default function Login() {
-  const [values, handleChange] = useFormState({
+  const [values, setValues] = useFormState({
     email: "",
-    password: "",
-    firstName: ""
+    password: ""
   });
+
+  useEffect(() => console.log("changed"), [values]);
 
   return (
     <div>
       <h2 className="title">Login (useEffect)</h2>
 
       <>
-        <input name="email" placeholder="email" value={values.email} onChange={handleChange} />
-        <input name="firstName" placeholder="first name" value={values.firstName} onChange={handleChange} />
-        <input type="password" name="password" value={values.password} onChange={handleChange} />
+        <input name="email" placeholder="email" value={values.email} onChange={setValues} />
+        <input type="password" name="password" value={values.password} onChange={setValues} />
       </>
     </div>
   );
